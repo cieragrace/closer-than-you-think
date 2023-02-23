093dc64239ed246ff184d6bf47a99b7d8e839701
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-23 14:23:25 -0700

    Add Sightings component skeleton

--- src/components/Sightings/Sightings.js
+++ src/components/Sightings/Sightings.js
@@ -1,1 +1,21 @@
+import React, { Component } from 'react'
 import './Sightings.css'
+import InfoCard from '../InfoCard/InfoCard'
+
+class Sightings extends Component {
+  constructor() {
+    super()
+    this.setState = {
+
+    }
+  }
+
+  render() {
+    return (
+      <></>
+    )
+  }
+  }
+
+
+export default Sightings
