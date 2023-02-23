fb3ad760fa461f667c6f8562545833c3fb98cb6c
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-23 13:56:24 -0700

    Add InfoCard component skeleton

---
+++ src/components/InfoCard/InfoCard.js
@@ -0,0 +1,17 @@
+import './InfoCard.css'
+import React from 'react'
+
+const InfoCard = () => {
+  return (
+    <div>
+      <div>
+        <h2></h2>
+      </div>
+      <div>
+       <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default InfoCard
